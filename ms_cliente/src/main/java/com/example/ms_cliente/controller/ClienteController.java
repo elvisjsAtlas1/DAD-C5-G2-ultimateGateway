dedc13b2a80034f8ab585abbc9857b24bb9240a4
ElvisjsAtlas <elvisjesusapazayucra15@gmail.com>
2025-04-10 09:02:07 -0500

    actualizando

--- ms_cliente/src/main/java/com/example/ms_cliente/controller/ClienteController.java
+++ ms_cliente/src/main/java/com/example/ms_cliente/controller/ClienteController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("/clientes")
 public class ClienteController {
     @Autowired
     private ClienteService clienteService;
